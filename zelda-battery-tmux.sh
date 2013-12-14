6c5d0194a4efee574d55f1d9a529f6a16a0bf237
Ryan Dlugosz <ryan@dlugosz.net>
2013-12-14 14:34:57 -0500

    Add the zelda battery to the tmux status line

---
+++ zelda-battery-tmux.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env zsh
+#
+# Works best with blinking text: the last heart will blink when you have less
+# than 25% of your battery life remaining.
+#
+# Add the following to your `~/.tmux.conf`;
+#
+#     set -g status-right "#[fg=red]#(path/to/zelda-battery-tmux.sh) "
+
+if (( $+commands[pmset] )) ; then
+  battery="$(pmset -g ps | awk 'NR==2' | perl -pe 's/.*?(\d+)%.*/\1/')"
+
+  if [[ $battery -lt 25 ]]; then
+    echo "#[blink]♥#[noblink]♡♡"
+  elif [[ $battery -lt 50 ]]; then
+    echo "♥♡♡"
+  elif [[ $battery -lt 75 ]]; then
+    echo "♥♥♡"
+  else
+    echo "♥♥♥"
+  fi
+fi
