8ab4949ca9a25f3a357a2bce2aa2ef43b64d74ae
Ryan Dlugosz <ryan@dlugosz.net>
2017-08-10 10:33:50 -0400

    Use variables to make changing chars easier

--- zelda-battery-tmux.sh
+++ zelda-battery-tmux.sh
@@ -9,18 +9,33 @@
 #
 #     set -g status-right "#[fg=red]#(path/to/zelda-battery-tmux.sh) "
 
+# Character reference
+# (to enter in Vim: C-v u####)
+# unicode - symbol
+# U+2665  - ♥
+# U+2661  - ♡
+# U+2764  - ❤
+# 
+# Note: we've played with using actual emoji chars in here, but it is
+# finicky in terms of how it is displayed in width. Also, some of the
+# emoji unicode codes collide with existing symbols in the font & that
+# version is displayed instead of the one from the emoji font. Unclear
+# if this is just the way it is or if we're doing something wrong.
+
 if (( $+commands[pmset] )) ; then
   battery="$(pmset -g ps | awk 'NR==2' | perl -pe 's/.*?(\d+)%.*/\1/')"
+  full=❤
+  empty=♡
 
   if [[ $battery -lt 20 ]]; then
-    echo "#[blink]♥#[noblink] ♡ ♡ ♡"
+    echo "#[blink] $full #[noblink] $empty $empty $empty"
   elif [[ $battery -lt 25 ]]; then
-    echo "♥ ♡ ♡ ♡"
+    echo "$full $empty $empty $empty"
   elif [[ $battery -lt 50 ]]; then
-    echo "♥ ♥ ♡ ♡"
+    echo "$full $full $empty $empty"
   elif [[ $battery -lt 75 ]]; then
-    echo "♥ ♥ ♥ ♡"
+    echo "$full $full $full $empty"
   else
-    echo "♥ ♥ ♥ ♥"
+    echo "$full $full $full $full"
   fi
 fi
